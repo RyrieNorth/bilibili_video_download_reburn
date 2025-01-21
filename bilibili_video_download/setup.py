2c8a29ddba21eb84491b34d6330f8b8e7f45f23a
NorthSky <53655849+RyrieNorth@users.noreply.github.com>
2025-01-21 13:49:48 +0800

    Add files via upload

---
+++ bilibili_video_download/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="bilibili_video_download_reburn_for_windows",
+    version="0.3",
+    license="MIT",
+    url="https://github.com/RyrieNorth/bilibili_video_download_reburn",
+    description="A tool for downloading and processing Bilibili videos",
+    platforms=["any"],
+    author="RyrieNorth",
+    author_email="bk15018708480@gmail.com",
+    install_requires=[
+        "requests",
+        "qrcode",
+    ],
+    py_modules=["bilibili_video_download"],
+    packages=["bilibili_video_download"],
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "bl_download = bilibili_video_download.cli:run",
+        ],
+    },
+)
