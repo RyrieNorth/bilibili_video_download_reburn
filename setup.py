2037c681ea73073ace24b32a65223cdb6bc90b05
NorthSky <53655849+RyrieNorth@users.noreply.github.com>
2025-01-21 14:49:43 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup
+
+if os.name == "nt":  # Windows 系统
+    package_data = {"": ["config.json", "tools/aria2c.exe", "tools/ffmpeg.exe"]}
+else:
+    package_data = {"": ["config.json", "tools/aria2c", "tools/ffmpeg"]}
 
 setup(
     name="bilibili_video_download_reburn",
@@ -16,6 +22,7 @@ setup(
     ],
     py_modules=["bilibili_video_download"],
     packages=["bilibili_video_download"],
+    package_data=package_data,
     include_package_data=True,
     entry_points={
         "console_scripts": [
