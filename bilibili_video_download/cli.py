00a39c92ce56e9a9ecf03354b573096ede296b86
NorthSky <53655849+RyrieNorth@users.noreply.github.com>
2025-01-23 09:39:19 +0800

    Update cli.py

--- bilibili_video_download/cli.py
+++ bilibili_video_download/cli.py
@@ -5,11 +5,11 @@ import argparse
 
 def run():
     parser = argparse.ArgumentParser(description="B站视频下载工具")
-    parser.add_argument("bv", type=str, help="视频的BV号")
+    parser.add_argument("video_id", type=str, help="视频的BV号或番剧的ep、ss号")
     args = parser.parse_args()
 
     # 调用 main 函数并传递 bv 参数
-    main(args.bv)
+    main(args.video_id)
 
 
 if __name__ == "__main__":
